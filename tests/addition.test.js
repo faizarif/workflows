151e022d55c15ed9cb172d3901b2f8cda9b3ed4c
Vivek Joshi <37882929+vivekjoshi556@users.noreply.github.com>
2023-04-02 01:33:59 +0530

    Messed up even more

--- tests/addition.test.js
+++ tests/addition.test.js
@@ -3,7 +3,7 @@ import add from "../index.js";
 describe("Test Add Function", () => {
     test.each([
         [1, 2, 5],
-        [2, 3, 5]
+        [2, 3, 6]
     ])("Addition Rules", (num1, num2, result) => {
         expect(add(num1, num2)).toBe(result);
     });
